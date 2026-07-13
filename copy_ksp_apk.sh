456f3bb35dd19ca81cfddc14653fb0791f48129b
alex <alex.chen@hnlens.com>
2026-07-13 13:56:13 +0800

    v1765
    Signed-off-by: alex <alex.chen@hnlens.com>

---
+++ copy_ksp_apk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp ../ksp/ksp/app/build/outputs/apk/dpc/release/app-dpc-release.apk  ./ksp/ksp.apk
+cp ../ksp/ksp/ksp.version   ./ksp/ksp.version
+cp ../ksp/ksp/parse.server  ./ksp/parse.server
+#git add ./*
+#git commit -sm "new-version"
+#git push -u origin HEAD:main
+
+
